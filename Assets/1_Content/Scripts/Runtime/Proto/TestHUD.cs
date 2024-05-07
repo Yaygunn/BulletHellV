cd4d69af8f143c93c4460704d483bdd0eab77a75
DeadShotNinja <Ukr816@gmail.com>
2024-05-06 19:07:31 -0600

    Added testing player controller

---
+++ Assets/1_Content/Scripts/Runtime/Proto/TestHUD.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BH.Runtime.Test
+{
+    public class TestHUD : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider _slider;
+        
+        private void Start()
+        {
+            _slider.value = 1f;
+        }
+        
+        public void SetSliderValue(float value)
+        {
+            _slider.value = value;
+        }
+        
+        public float GetSliderValue()
+        {
+            return _slider.value;
+        }
+    }
+}
