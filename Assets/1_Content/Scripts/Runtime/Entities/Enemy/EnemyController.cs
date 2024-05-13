589c7a544be2eaf7fbc24175d9f83f2fd49e95b4
Glatrix <downeyryan1705@gmail.com>
2024-05-12 22:53:22 -0500

    Basic Enemy Controller/State

---
+++ Assets/1_Content/Scripts/Runtime/Entities/Enemy/EnemyController.cs
@@ -0,0 +1,89 @@
+using BH.Runtime.Managers;
+using BH.Runtime.StateMachines;
+using BH.Runtime.Systems;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+using Zenject;
+
+namespace BH.Runtime.Entities
+{
+    public class EnemyController : Entity, IDamageable
+    {
+        [field: FoldoutGroup("Health"), SerializeField, HideLabel]
+        public Health Health { get; private set; }
+
+        public Transform AttackTarget { get; private set; }
+
+
+        [field: BoxGroup("Debug"), SerializeField, ReadOnly]
+        public string StateName { get; set; }
+
+        [Inject]
+        private LevelManager _levelManager;
+
+        // TODO: Add Enemy Signals
+        //[Inject]
+        //private SignalBus _signalBus;
+
+        #region Components
+        public MovementComponent Movement { get; private set; }
+        #endregion
+
+        #region State Machine
+        public StateMachine<EnemyState> EnemyHFSM { get; private set; }
+
+        public EnemyIdleState IdleState { get; private set; }
+        public EnemyChaseState ChaseState { get; private set; }
+
+        #endregion State Machine
+
+        #region Unity Callbacks
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Movement = VerifyComponent<MovementComponent>();
+
+            EnemyHFSM = new StateMachine<EnemyState>();
+
+            // Active States
+            IdleState = new EnemyIdleState(this, EnemyHFSM);
+            ChaseState = new EnemyChaseState(this, EnemyHFSM);
+
+            EnemyHFSM.Initialize(IdleState);
+
+            // Plan to move to Enemy Spawner
+            Health.ResetHealth();
+        }
+
+        private void Update()
+        {
+            EnemyHFSM.CurrentState.LogicUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            EnemyHFSM.CurrentState.PhysicsUpdate();
+        }
+
+        #endregion
+
+        public void Damage(int ammount)
+        {
+            Health.TakeDamage(ammount);
+        }
+
+        // TODO: Impliment Health Features
+        //private void OnHealthChanged(int maxHealth, int currentHealth)
+        //private void OnDied()
+
+        #region Unity Callbacks
+        private void Start()
+        {
+            AttackTarget = _levelManager.Player.transform;
+        }
+        #endregion
+    }
+}
