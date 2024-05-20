42889b3ec6e587afbfb705db6c0ba195607f2342
toffl1 <fhaytr63@gmail.com>
2024-05-20 02:55:57 +0200

    fixed bullet visual in the top right ui
    + fixed homing bullet scaling

--- Assets/1_Content/Scripts/Runtime/UI/PlayerHUD/BulletVisual.cs
+++ Assets/1_Content/Scripts/Runtime/UI/PlayerHUD/BulletVisual.cs
@@ -8,17 +8,25 @@ namespace BH.Runtime.UI
     {
         private Image _bulletImage;
         private TMP_Text _levelText;
+        private RectTransform _rectTransform;
         
         private void Awake()
         {
             _bulletImage = GetComponent<Image>();
             _levelText = GetComponentInChildren<TMP_Text>();
+            _rectTransform = GetComponent<RectTransform>();
         }
 
         public void SetBulletVisual(Sprite sprite, int level)
         {
             _bulletImage.color = Color.white;
             _bulletImage.sprite = sprite;
+
+            //reset scale while maintaining width
+            _bulletImage.SetNativeSize();
+            float aspectRatio = _rectTransform.rect.height / _rectTransform.rect.width; 
+            _rectTransform.sizeDelta = new Vector2 (50, 50 * aspectRatio);
+
             
             if (level != 0)
                 _levelText.text = level.ToString();
